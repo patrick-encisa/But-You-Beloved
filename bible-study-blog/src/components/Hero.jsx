109547857578cb7716f86e9556e0171a561e7741
patrick-encisa <81599745+patrick-encisa@users.noreply.github.com>
2024-03-08 15:41:18 -0800

    Initial commit

---
+++ bible-study-blog/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import heroImage from "../assets/Frame 1.png"; // Updated path
+
+function Hero() {
+  return (
+    <div className="Hero">
+      <img src={heroImage} alt="Hero" className="animate-fadeIn" />
+      <div>
+        <h1 className="animate-fadeIn shadows-into-light-regular ">
+          building up yourselves upon your most holy faith, praying in the Holy
+          Spirit, Keep yourselves in the love of God, awaiting the mercy of our
+          Lord Jesus Christ unto eternal life. - Jude 20 and 21
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ bible-study-blog/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 2s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fontFamily: {
+          "shadows-into-light": ['"Shadows Into Light"', "cursive"],
+        },
+      },
+    },
+  },
+  plugins: [],
+};
