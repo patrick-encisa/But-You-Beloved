2aa14a8b78dc46e9324c3ab69f106a3e658869f1
patrick-encisa <81599745+patrick-encisa@users.noreply.github.com>
2024-03-08 16:50:27 -0800

    Made first blog post
    Copy and pasted the first blog post

--- bible-study-blog/src/data/BlogData.jsx
+++ bible-study-blog/src/data/BlogData.jsx
@@ -2,11 +2,38 @@
 const blogPosts = [
   {
     id: 1,
-    title: "My First Blog Post",
-    author: "John Doe",
-    date: "March 8, 2024",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    title: "Introduction",
+    author: "P.O.E",
+    date: "3/8/2024",
+    content: (
+      <p>
+        Hi! I intend to use this blog to document my spiritual journey and
+        growth. I intend to do in depth, but also not too boring, blog posts on
+        my learnings and takeaways from every book and chapter of the Bible. I
+        plan on starting in the New Testament first, from Matthew to Revelation.
+        Then, I want to move on to the Old Testament. But first, a little about
+        me. I first gained the Lord at the age of 8, when a lyric from a hymnal
+        brought me to tears. The lyric went: Lord, take my life, I present it to
+        You! If I had a thousand, I'd pour all on You! Nothing withholding, my
+        all is for You. My life and my future, dear Lord, is all for You. Of
+        course, at the age of 8, I didn't have a full realization of what these
+        words would really mean. It wasn't until years later that I would
+        understand the true cost, the value, and the weight of these words. All
+        I knew then was that tears were falling down my face. I knew, at the age
+        of 8, that I felt something real. I got baptized in 2019, on the same
+        day as my birthday. I chose that specific date, my birthday, to
+        commemorate both my human birth and spiritual re- birth. Since then,
+        I've had many ups and downs in my Christian life. I want to use this
+        blog as my own documentation of where I am, and hopefully you may gain
+        something too. I don't yet know how often I will post: hopefully as much
+        as possible with time permitting. I truly hope that this blog encourages
+        me to stay in the Word of God. Oh! I almost forgot. As to the name of
+        the blog, "But you, Beloved" comes from Jude 1:20. The Lord has recently
+        put the writings of Jude in my heart, and I've been musing on it for the
+        past few weeks or so. I just thought it sounded fitting for a blog name,
+        and I hope you can agree!
+      </p>
+    ),
   },
   {
     id: 2,
